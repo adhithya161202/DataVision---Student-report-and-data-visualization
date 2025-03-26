87a00418f1fce512b2738b053fcd6b66b80d2fe7
aneetaaa <122656340+aneetaaa@users.noreply.github.com>
2025-03-27 04:46:31 +0530

    Add files via upload

--- frontend/src/components/pie3.js
+++ frontend/src/components/pie3.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
+
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const StudentsPieChart = () => {
