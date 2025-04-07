14f582d62699023f0959692116d89b66794808d0
adhithya161202 <adhithyaofficial0113@gmail.com>
2025-04-07 18:09:29 +0530

    updated linegraph

--- frontend/src/components/Line.js
+++ frontend/src/components/Line.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart, registerables } from 'chart.js';
 
-
-
 Chart.register(...registerables); 
 
 export const LineGraph = () => {
